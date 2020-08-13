a3c85fa6988574572b37ac04e2d265fc36195517
Erik Berthold <eberthold@users.noreply.github.com>
2020-08-13 08:25:27 +0200

    deleteMergedBranches.ps1 now asks for deletion
    it also displays what to be deleted beforehand

--- Git/deleteMergedBranches.ps1
+++ Git/deleteMergedBranches.ps1
@@ -1,1 +1,9 @@
-git branch -d @(git branch --merged | %{ $_.Trim() } | Select-String -Pattern @(git rev-parse --abbrev-ref HEAD) -notmatch)
+$branches = git branch --merged | %{ $_.Trim() } | Where-Object { $_ -ne "master" }
+Write-Output "Delete following branches?"
+Write-Output $branches
+$choice = Read-Host '(y/n)'
+if ($choice -eq 'y')
+{
+    git branch -d @($branches | Select-String -Pattern @(git rev-parse --abbrev-ref HEAD) -notmatch)
+    Write-Output "branches deleted"
+}
